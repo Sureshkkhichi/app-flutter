adb92081b7b8a6f5038c60831250fa1f2cfe7202
Sureshkkhichi <sureshk.khichi@gmail.com>
2021-09-08 17:46:14 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,59 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
     title: "Awesome App",
     home: HomePage(),
+    theme: ThemeData(primarySwatch: Colors.purple),
   ));
 }
 
 class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Awesome app"),
+        title: Text('Awesome App'),
       ),
-      body: Container(
-        child: Center(child: Text("Hi Flutter")),
+      body: Container(),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            // DrawerHeader(
+            //   child: Text("Hi, I am drawer",
+            //       style: TextStyle(color: Colors.white)),
+            //   decoration: BoxDecoration(color: Colors.purple),
+            // ),
+            UserAccountsDrawerHeader(
+                accountName: Text("Suresh Kumar"),
+                accountEmail: Text("sureshk.khichi@gmail.com"),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80"),
+                )),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Account"),
+              // subtitle: Text("Personal"),
+              trailing: Icon(Icons.edit),
+            ),
+            ListTile(
+              leading: Icon(Icons.email),
+              title: Text("Email"),
+              // subtitle: Text("Personal"),
+              trailing: Icon(Icons.send),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.edit),
       ),
     );
   }
